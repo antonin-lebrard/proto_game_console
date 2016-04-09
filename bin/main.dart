cacdb93978bb094961a0cdc40071811bd0bca971
Antonin <a.lebrard@gmail.com>
2016-04-09 12:28:49 +0200

    [wip] moved console to lib repository for future tests building

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:proto_game/src/proto_game_base.dart';
 import 'package:proto_game_console/command.dart';
-import 'Console/console_entryPoint.dart';
+import 'package:proto_game_console/console/console_entryPoint.dart';
 
 main(List<String> args) async {
   String jsonContent = new File('example.json').readAsStringSync();
